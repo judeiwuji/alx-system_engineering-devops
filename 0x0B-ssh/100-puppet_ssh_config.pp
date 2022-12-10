2c0c21872e9bbc1735cecafd928f65f7090751de
jude iwuji <judeiwuji@gmail.com>
2022-12-10 15:54:14 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,16 @@
 file{'/etc/ssh/ssh-config':
 ensure  => 'present',
 content => 'Host 63960-web-01
-  User ubuntu
-  IdentityFile ~/.ssh/school
-  IdentitiesOnly yes'
+User ubuntu
+'
+}
+
+exec{'Turn off passwd auth':
+  command => '/usr/bin/echo "IdentitiesOnly yes" >> /etc/ssh/ssh-config',
+  require => File['/etc/ssh/ssh-config']
+}
+
+exec{'Declare identity file':
+  command => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh-config',
+  require => File['/etc/ssh/ssh-config']
 }
