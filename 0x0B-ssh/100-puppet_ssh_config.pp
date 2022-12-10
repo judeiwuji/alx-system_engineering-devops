97e07c2e57395638616bb824343ec756e7769529
jude iwuji <judeiwuji@gmail.com>
2022-12-10 16:48:53 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -17,7 +17,7 @@ file_line {'Turn off passwd auth':
 file_line {'Declare identity file':
   ensure  => 'present',
   line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile'
+  match   => '^IdentityFile',
   path    => '/etc/ssh/ssh-config',
   require => File['/etc/ssh/ssh-config']
 }
