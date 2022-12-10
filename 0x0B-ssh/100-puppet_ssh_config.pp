7a80209d90059332257436319deb435ca43579c6
jude iwuji <judeiwuji@gmail.com>
2022-12-10 15:38:25 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,8 @@
 # Client configuration file (w/ Puppet)
-exec{'ssh config':
-command => '/usr/bin/echo -e "Host 63960-web-01\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tIdentitiesOnly yes" >> /etc/ssh/ssh-config'
+file{'/etc/ssh/ssh-config':
+ensure  => 'present',
+content => 'Host 63960-web-01
+  User ubuntu
+  IdentityFile ~/.ssh/school
+  IdentitiesOnly yes'
 }
