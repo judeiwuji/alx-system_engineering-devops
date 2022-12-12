42134277495594e80d3cb9052f8525822aebde18
jude iwuji <judeiwuji@gmail.com>
2022-12-12 07:00:31 +0100

    correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,19 @@
 # Client configuration file (w/ Puppet)
 
-file{'/etc/ssh/ssh-config':
+file{'/etc/ssh/ssh_config':
   ensure  => 'file',
 }
 -> file_line {'Turn off passwd auth':
   ensure  => 'present',
   line    => 'IdentitiesOnly yes',
   match   => '^IdentitiesOnly',
-  path    => '/etc/ssh/ssh-config',
-  require => File['/etc/ssh/ssh-config']
+  path    => '/etc/ssh/ssh_config',
+  require => File['/etc/ssh/ssh_config']
 }
 -> file_line {'Declare identity file':
   ensure  => 'present',
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^IdentityFile',
-  path    => '/etc/ssh/ssh-config',
-  require => File['/etc/ssh/ssh-config']
+  path    => '/etc/ssh/ssh_config',
+  require => File['/etc/ssh/ssh_config']
 }
