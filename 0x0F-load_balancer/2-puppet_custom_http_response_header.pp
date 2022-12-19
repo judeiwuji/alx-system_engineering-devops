e6ef08e5e0aaa4fecb6d16116e11b4ac4cbbc382
jude iwuji <judeiwuji@gmail.com>
2022-12-19 18:10:34 +0100

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,14 +4,19 @@ exec {'sudo chown -R':
   command => "/usr/bin/sudo /bin/chown -R ${id}:${id} /etc/nginx/sites-available/"
 }
 
+service {'sudo service nginx':
+  restart => '/usr/bin/sudo service nginx restart'
+}
+
 file {'/etc/nginx/sites-available/default':
   ensure => 'file'
 }
 
 -> file_line{'X-Served-By':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  line   => '    server_name _;
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  line    => '    server_name _;
     add_header X-Served-By $hostname;',
-  match  => 'server_name _;'
+  match   => 'server_name _;',
+  require => Service['sudo service nginx']
 }
