462a352ad671315e879d91fe662a680f3b4907f8
jude iwuji <judeiwuji@gmail.com>
2022-12-19 16:22:35 +0100

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,9 @@
 # automate the task of creating a custom HTTP header response
+
+exec {'sudo chown -R':
+  command => "/usr/bin/sudo /usr/bin/chown -R ${facts['user']}:${facts['user']} /etc/nginx/sites-available/"
+}
+
 file {'/etc/nginx/sites-available/default':
   ensure => 'file'
 }
