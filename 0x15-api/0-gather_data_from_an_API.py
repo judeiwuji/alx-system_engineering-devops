8c65a25279a33e45ec434622cb658cb7c450e516
jude iwuji <judeiwuji@gmail.com>
2023-01-16 17:46:26 -0600

    Added task 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+import requests
+import sys
+import json
+
+
+def main():
+    """Entry point"""
+    user_id = int(sys.argv[1])
+    res = requests.get("https://jsonplaceholder.typicode.com/user/{:d}/todos".
+                       format(user_id))
+    res1 = requests.get("https://jsonplaceholder.typicode.com/users/{:d}".
+                        format(user_id))
+    if res.status_code == 200 and res1.status_code == 200:
+        tasks = json.loads(res.content)
+        user = json.loads(res1.content)
+        completed_tasks = []
+        total_completed = 0
+        total_tasks = 0
+
+        for task in tasks:
+            if task['completed']:
+                total_completed += 1
+                completed_tasks.append(task)
+            total_tasks += 1
+        print("Employee {} is done with tasks({:d}/{:d})".
+              format(user['name'], total_completed, total_tasks))
+        for task in completed_tasks:
+            print("\t {}".format(task['title']))
+
+
+if __name__ == '__main__':
+    main()
