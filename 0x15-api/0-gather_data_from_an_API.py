17a3612cfbbc77292b9e2aa2ba1547c84c62b95f
jude iwuji <judeiwuji@gmail.com>
2023-01-16 18:17:40 -0600

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,6 @@
 using this REST API, for a given employee ID, returns information
 about his/her TODO list progress.
 """
-import json
 import requests
 import sys
 
@@ -16,8 +15,8 @@ def main():
     res1 = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(user_id))
     if res.status_code == 200 and res1.status_code == 200:
-        tasks = json.loads(res.content)
-        user = json.loads(res1.content)
+        tasks = res.json()
+        user = res1.json()
         completed_tasks = []
         total_completed = 0
         total_tasks = 0
