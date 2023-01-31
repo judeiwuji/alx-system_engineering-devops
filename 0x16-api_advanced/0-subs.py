139b29414d60b4af0b9613c483bfa6ed7fac1e00
jude iwuji <judeiwuji@gmail.com>
2023-01-31 12:36:40 -0800

    style update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Returns number of subscribers in a reddit"""
+    """Returns number of subscribers in a reddit"""
 
-	api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {'User-Agent': 'Alx-Project by jude'}
-	response = requests.get(api, headers=headers, allow_redirects=False)
+    api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Alx-Project by jude'}
+    response = requests.get(api, headers=headers, allow_redirects=False)
 
-	if response.status_code == 200:
-		record = response.json().get('data')
-		return record.get('subscribers')
-	return 0
+    if response.status_code == 200:
+        record = response.json().get('data')
+        return record.get('subscribers')
+    return 0
