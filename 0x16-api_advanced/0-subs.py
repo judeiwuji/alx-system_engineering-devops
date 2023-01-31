198dce3bb0cf3776f637e250ca4760684ff018ad
jude iwuji <judeiwuji@gmail.com>
2023-01-31 12:35:26 -0800

    Add: Task 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Count subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+	"""Returns number of subscribers in a reddit"""
+
+	api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {'User-Agent': 'Alx-Project by jude'}
+	response = requests.get(api, headers=headers, allow_redirects=False)
+
+	if response.status_code == 200:
+		record = response.json().get('data')
+		return record.get('subscribers')
+	return 0
